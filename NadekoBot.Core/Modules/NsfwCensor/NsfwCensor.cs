0e7b5054fce168233165ca7fc8c17d221ee33ac4
Quan Pham <doraemonfanclub@gmail.com>
2020-05-26 18:44:18 +0700

    Minor fix

--- NadekoBot.Core/Modules/NsfwCensor/NsfwCensor.cs
+++ NadekoBot.Core/Modules/NsfwCensor/NsfwCensor.cs
@@ -1,12 +1,16 @@
 ﻿using NadekoBot.Core.Services;
+using NLog;
 
 namespace NadekoBot.Modules.NsfwCensor
 {
     public class NsfwCensor : NadekoTopLevelModule<IClarifaiService>
     {
+        private static Logger _log = LogManager.GetCurrentClassLogger();
+
         private readonly IBotCredentials _creds;
         public NsfwCensor()
         {
+            _log.Error("Test test");
         }
     }
 }
--- NadekoBot.Core/Services/IClarifaiService.cs
+++ NadekoBot.Core/Services/IClarifaiService.cs
@@ -1,11 +1,8 @@
 ﻿using Clarifai.API;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace NadekoBot.Core.Services
 {
-    public interface IClarifaiService : INService
+  public interface IClarifaiService : INService
     {
         ClarifaiClient ClarifaiClient { get; }
     }
