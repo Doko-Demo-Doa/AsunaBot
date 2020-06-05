1bcd6cc574f6ee0305c245e5d973ae08f54768cc
binhlt <binhlt@gtv.com.vn>
2020-06-05 12:28:31 +0700

    add script for a view

---
+++ scripts.sql
@@ -0,0 +1,11 @@
+﻿-- public."VLeaderboards" source
+
+CREATE OR REPLACE VIEW public."VLeaderboards"
+AS SELECT (u."Username" || '#'::text) || u."Discriminator" AS "Username",
+    l."UserId",
+    l."Type",
+    l."TimeType",
+    l."Date",
+    l."Score"
+   FROM "Leaderboards" l
+     JOIN "DiscordUsers" u ON l."UserId" = u."UserId";
