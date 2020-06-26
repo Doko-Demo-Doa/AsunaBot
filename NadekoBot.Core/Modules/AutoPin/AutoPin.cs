2b5faa7e47935fc6cff1d3fc2037b47d589421cb
Quan Pham <doraemonfanclub@gmail.com>
2020-06-26 23:03:22 +0700

    Add autopin module

---
+++ NadekoBot.Core/Modules/AutoPin/AutoPin.cs
@@ -0,0 +1,23 @@
+﻿using Discord;
+using Discord.WebSocket;
+using NadekoBot.Modules;
+using System.Threading.Tasks;
+
+namespace NadekoBot.Modules.AutoPin
+{
+    class AutoPin : NadekoTopLevelModule
+    {
+        private readonly DiscordSocketClient _client;
+
+        public AutoPin(DiscordSocketClient client)
+        {
+            _client = client;
+            _client.ReactionAdded += this.OnReactionAdded;
+        }
+
+        private Task OnReactionAdded(Cacheable<IUserMessage, ulong> msg, ISocketMessageChannel chan, SocketReaction reaction)
+        {
+            return null;
+        }
+    }
+}
