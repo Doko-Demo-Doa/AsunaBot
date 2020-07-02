43673e87e4ed07554984b1c38b43bcfbb4e15066
Quan Pham <doraemonfanclub@gmail.com>
2020-07-02 13:10:44 +0700

    Fix minor bug

--- NadekoBot.Core/Modules/PinMessage/PinMessage.cs
+++ NadekoBot.Core/Modules/PinMessage/PinMessage.cs
@@ -4,6 +4,7 @@ using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using NadekoBot.Modules;
+using NLog;
 
 namespace NadekoBot.Core.Modules.PinMessage
 {
@@ -11,19 +12,22 @@ namespace NadekoBot.Core.Modules.PinMessage
     public class PinMessage : NadekoTopLevelModule
     {
         private readonly DiscordSocketClient _client;
-        private readonly int PIN_THRESHOLD = 5;
+        private readonly int PIN_THRESHOLD = 1;
         private readonly string PIN_EMOTE = "\uD83D\uDCCC";
+
+        private static Logger _log;
 
         public PinMessage(DiscordSocketClient client)
         {
             _client = client;
             _client.ReactionAdded += this.OnReactionAdded;
             _client.ReactionRemoved += this.OnReactionRemoved;
+            _log = LogManager.GetCurrentClassLogger();
         }
 
         private async Task OnReactionAdded(Cacheable<IUserMessage, ulong> msg, ISocketMessageChannel chan, SocketReaction reaction)
         {
-            if (reaction.Emote.Name == "")
+            if (reaction.Emote.Name.Equals(PIN_EMOTE))
             {
                 IUserMessage m = (IUserMessage)await chan.GetMessageAsync(msg.Id);
                 var ReactionList = m.Reactions;
@@ -39,7 +43,7 @@ namespace NadekoBot.Core.Modules.PinMessage
 
         private async Task OnReactionRemoved(Cacheable<IUserMessage, ulong> msg, ISocketMessageChannel chan, SocketReaction reaction)
         {
-            if (reaction.Emote.Name == "")
+            if (reaction.Emote.Name.Equals(PIN_EMOTE))
             {
                 IUserMessage m = (IUserMessage)await chan.GetMessageAsync(msg.Id);
                 var ReactionList = m.Reactions;
