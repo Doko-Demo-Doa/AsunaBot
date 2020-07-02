a9e6a6d0355717380905907efa24f4e8cdbc1df0
Quan Pham <doraemonfanclub@gmail.com>
2020-07-02 13:25:25 +0700

    Add Streaming Module

---
+++ NadekoBot.Core/Modules/StreamingDetector/StreamingDetectorModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using NadekoBot.Modules;
+
+namespace NadekoBot.Core.Modules.StreamingDetector
+{
+    public class StreamingDetectorModule : NadekoTopLevelModule
+    {
+        private readonly DiscordSocketClient _client;
+
+        public StreamingDetectorModule(DiscordSocketClient client)
+        {
+            _client = client;
+            _client.UserVoiceStateUpdated += OnVoiceStateUpdated;
+        }
+
+        private Task OnVoiceStateUpdated(SocketUser usr, SocketVoiceState oldState, SocketVoiceState newState)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
